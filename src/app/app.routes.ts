7ef5b2874fa83361c8d84a9b4f6a20363e4d10b6
Triza Ominah <trizaminah@gmail.com>
2025-11-28 14:03:08 +0300

    errors to be solved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { SignUp } from './components/sign-up/sign-up';
 import { SignIn } from './components/sign-in/sign-in'; 
+import { About } from './components/about/about';
+
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'sign-up', component: SignUp },
   { path: 'sign-in', component: SignIn }, 
+  { path: 'about', component: About },
 ];
