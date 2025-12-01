270a4c637102ccbb792d6f1a11c82e97f0e3959b
Triza Ominah <trizaminah@gmail.com>
2025-12-01 14:38:19 +0300

    some fixes

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../navbar/navbar';
+import { NavbarComponent } from '../navbar/navbar';
 import { HeroComponent } from '../hero/hero';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Navbar, HeroComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
