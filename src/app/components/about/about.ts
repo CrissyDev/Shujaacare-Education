e4e95bd6db0a052826548735076efb8a3061035d
Triza Ominah <trizaminah@gmail.com>
2025-12-02 15:55:33 +0300

    mission

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Mission } from '../mission/mission';
 
 @Component({
   selector: 'app-about',
   standalone: true, 
-  imports: [],
+  imports: [CommonModule, Mission],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
 
+  constructor(private router: Router) {}
 }
